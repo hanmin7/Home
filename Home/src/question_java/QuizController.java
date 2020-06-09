e3dc789fb6a4eca8888cf73113020ad768cdab26
LGPC <LGPC@DESKTOP-G15CLQ0>
2020-06-09 20:13:38 +0900

    0609_quiz_중복확인완료토글티생성

--- Home/src/question_java/QuizController.java
+++ Home/src/question_java/QuizController.java
@@ -22,6 +22,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -34,6 +36,10 @@ public class QuizController implements Initializable {
 	Button btnOk, btnNext, btnClose;
 	@FXML
 	Label select1, select2, select3, quiz;
+	@FXML
+	RadioButton rad1, rad2, rad3;
+	@FXML
+	ToggleGroup group;
 
 	ObservableList<Quiz> alist, blist;
 
@@ -48,17 +54,42 @@ public class QuizController implements Initializable {
 			e.printStackTrace();
 		}
 
-//		alist = getQuizList();
-		blist = getQuizList();
-		 
-//		for(int i=0; i<alist.size(); i++) {
-//			Random random = new Random();
-//			int ranNum = random.nextInt(alist.size());
-//			 
-		//중복처리 넣기
-//			
-//			blist.add(alist.get(ranNum));
-//		}
+		blist = FXCollections.observableArrayList();
+		alist = getQuizList();
+//		for(Quiz q : alist)
+//		System.out.println(q);
+//		blist = getQuizList();
+		
+		
+		int[] intAry = new int[alist.size()];
+		int aryLength = intAry.length;
+
+		for (int i = 0; i < aryLength;) {
+			int temp = (int) (Math.random() * aryLength);
+//			System.out.println("temp: " + temp);
+			intAry[i] = temp;
+			int j = i;
+			for (; j > 0;) {
+				if (i != 0) {
+					if (intAry[i] == intAry[j - 1]) {
+						break;
+					}
+					j--;
+				}
+			}
+			if (j != 0)
+				continue;
+			i++;
+		}//intAry 중복처리  인덱스값 랜덤
+		
+		
+//		for(int i=0; i<5; i++)
+//			System.out.println(intAry[i]);
+		
+		for (int i = 0; i < alist.size(); i++) {
+
+			blist.add(alist.get(intAry[i]));
+		}
 
 		getAnsList(blist.get(0));
 
@@ -74,7 +105,7 @@ public class QuizController implements Initializable {
 	public ObservableList<Quiz> getQuizList() {
 		ObservableList<Quiz> list = FXCollections.observableArrayList();
 //		Random random = new Random();
-//		numi = random.nextInt(5) + 1;
+//		numi = random.nextInt(5) + 1;   
 		String sql = "select num, quiz, select1, select2, select3, r1, r2, r3, answer, answerlist  from question";
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(sql);
@@ -85,7 +116,8 @@ public class QuizController implements Initializable {
 						rs.getInt("r3"), rs.getString("answer"), rs.getString("answerlist"));
 				list.add(board);
 			}
-
+			for(Quiz q : list)
+				System.out.println(q);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -118,13 +150,13 @@ public class QuizController implements Initializable {
 				}
 			}
 		}
-
 		quiz.setText(quizAns.getQuiz());
 		select1.setText(quz[num1]);
 		select2.setText(quz[num2]);
 		select3.setText(quz[num3]);
-	}
+	} //getAnsList 문제에 대한 보기들 랜덤 뿌리기
 
+	
 	public void handleBtnOkAction(ActionEvent ae) {
 		Stage addStage = new Stage(StageStyle.UTILITY);
 		addStage.initModality(Modality.WINDOW_MODAL);
@@ -148,7 +180,7 @@ public class QuizController implements Initializable {
 						getAnsList(blist.get(nextNum++));
 					} catch (IndexOutOfBoundsException e) {
 						System.out.println("ㅊㅋㅊㅋ");
-						//문제 다 돌렸을 때 구현 넣기
+						// 문제 다 돌렸을 때 구현 넣기
 					}
 
 				}
