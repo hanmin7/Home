3add317cd950c89af90c44edf7b0ce28185b1b48
LGPC <LGPC@DESKTOP-G15CLQ0>
2020-06-15 06:54:58 +0900

    0613_팝업추가

--- Home/src/question_java/QuizController.java
+++ Home/src/question_java/QuizController.java
@@ -20,6 +20,8 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -27,7 +29,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Modality;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -80,7 +87,7 @@ public class QuizController implements Initializable {
 						break;
 					}
 					j--;
-				} 
+				}
 			}
 			if (j != 0)
 				continue;
@@ -106,13 +113,13 @@ public class QuizController implements Initializable {
 		group.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
 			@Override
 			public void changed(ObservableValue<? extends Toggle> observable, Toggle oldVal, Toggle newVal) {
-				if (newVal != null) {//newVal에 담는데 밑에서 deselect해서 널값으로 넘어옴. 그래서 토글누르면 값이널값이 아니게되니까 널값이 아닐 때
+				if (newVal != null) {// newVal에 담는데 밑에서 deselect해서 널값으로 넘어옴. 그래서 토글누르면 값이널값이 아니게되니까 널값이 아닐 때
 					int test = 1;
 					if (newVal.getUserData().equals(select1.getUserData())) {
 //						System.out.println("1");
-						//내가 누른버튼의 getUserData랑 라벨1의 getUserData랑 묶고
-						//라벨1의 getText랑 blist에서 가져오는 보기1번이랑 비교. getSelect1이 답이 들어있으니 같으면 정답. 다르면 오답.
-						//그래서 라벨1이랑 묶여서 보기1,2,3비교하고 라벨2랑묶어서 보기1,2,3비교하고 라벨3이랑 묶어서 보기1,2,3 비교.
+						// 내가 누른버튼의 getUserData랑 라벨1의 getUserData랑 묶고
+						// 라벨1의 getText랑 blist에서 가져오는 보기1번이랑 비교. getSelect1이 답이 들어있으니 같으면 정답. 다르면 오답.
+						// 그래서 라벨1이랑 묶여서 보기1,2,3비교하고 라벨2랑묶어서 보기1,2,3비교하고 라벨3이랑 묶어서 보기1,2,3 비교.
 						// 같은식으로 반복돌리는데 좀더코드를 줄이는 방법은??
 						if (select1.getText().equals(blist.get(nextAnum).getSelect1())) {
 //							System.out.println("정답");
@@ -157,11 +164,9 @@ public class QuizController implements Initializable {
 
 		});
 
-		
 		scoreLabel.setText(Integer.toString(score));
-		
-		
-	} //initialize
+
+	} // initialize
 
 	public ObservableList<Quiz> getQuizList() {
 		ObservableList<Quiz> list = FXCollections.observableArrayList();
@@ -234,7 +239,6 @@ public class QuizController implements Initializable {
 			answerLabel.setText(match);
 			// 정답매칭 후 정답이다 오답이다뿌려주기
 
-			
 			Label answerList = (Label) parent.lookup("#answerList");
 			answerList.setText(blist.get(nextAnum++).getAnswerlist());
 
@@ -249,14 +253,16 @@ public class QuizController implements Initializable {
 					} else if (rad3.isSelected()) {
 						rad3.setSelected(false);
 					}
-					
+
 					addStage.close();
 					scoreLabel.setText(Integer.toString(score));
-					
+
 					try {
 						getAnsList(blist.get(nextNum++));
 					} catch (IndexOutOfBoundsException e) {
 						System.out.println("ㅊㅋㅊㅋ");
+//						messagePopup("모든 문제를 다 풀었습니다!!짝짝짝!!");
+						messageDialog("모든 문제를 다 풀었습니다!! 짝짝짝!!");
 						// 문제 다 돌렸을 때 구현 넣기
 					}
 
@@ -282,5 +288,73 @@ public class QuizController implements Initializable {
 		Platform.exit();
 
 	}
+
+	
+	
+	public void messageDialog(String message) {
+		Stage customStage = new Stage(StageStyle.UTILITY);
+		customStage.initModality(Modality.WINDOW_MODAL);
+		customStage.initOwner(btnOk.getScene().getWindow());
+		customStage.setTitle("ㅊㅋㅊㅋ");
+		
+		AnchorPane ap = new AnchorPane();
+		ap.setPrefSize(400, 150);
+		
+		Button button = new Button("종료");
+		button.setLayoutX(336);
+		button.setLayoutY(104);
+//		button.setOnAction(e->customStage.close());
+		button.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				Platform.exit();
+			}
+		});
+		
+		
+		Label label = new Label(message);
+		label.setLayoutX(87);
+		label.setLayoutY(33);
+		label.setPrefHeight(15);
+		label.setPrefWidth(290);
+		
+		ap.getChildren().add(button);
+		ap.getChildren().add(label);
+
+		Scene scene = new Scene(ap);
+		customStage.setScene(scene);
+		customStage.show();
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+//	public void messagePopup(String message) {
+//
+//		// 컨테이너(HBox) 생성.
+//		HBox hbox = new HBox();
+//		hbox.setStyle("-fx-background-color:black; -fx-background-radius: 5;");
+//		hbox.setAlignment(Pos.CENTER);
+//
+//		// 컨트롤(Label)
+//		Label label = new Label();
+//		HBox.setMargin(label, new Insets(5, 5, 5, 5));
+//		label.setText(message);
+//		label.setStyle("-fx-text-fill:white;");
+//
+//		// 컨테이너에 컨트롤 담기
+//		hbox.getChildren().add(label);
+//
+//		// 팝업 생성. 컨테이너 담아서 팝업 호출.
+//		Popup popup = new Popup();
+//		popup.getContent().add(hbox);
+//		popup.setAutoHide(true); // 포커스가 다른곳으로 가면 사라짐.
+//		popup.show(btnOk.getScene().getWindow()); // 윈도우를 가져와야함
+//
+//	}
 
 }
